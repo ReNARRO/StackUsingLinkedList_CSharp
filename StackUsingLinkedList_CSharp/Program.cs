16462faa8b8d34385952dba0633b25cf0b2190e3
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2022-11-30 16:06:03 +0700

    Menambahkan fungsi while untuk menampilkan pilihan dan mendeklarasikan variabel inpu dan ch

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -63,6 +63,20 @@ namespace StackUsingLinkedList_CSharp
             }
             static void Main(string[] args)
             {
+                Stacks s = new Stacks();
+                while (true)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("\n ***Stack Menu***\n");
+                    Console.WriteLine("1. Push");
+                    Console.WriteLine("2. Pop");
+                    Console.WriteLine("3. Display");
+                    Console.WriteLine("4. Exit");
+                    Console.WriteLine("\n Enter your choice : ");
+                    string input = Console.ReadLine();
+                    char ch = Convert.ToChar(input == "" ? "0" : input);
+                
+                }
 
             }
         }
